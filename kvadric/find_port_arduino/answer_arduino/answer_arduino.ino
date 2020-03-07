005e986db44dda5e8667fcf4ca61fb495b6cd841
honepa <cot1884@gmail.com>
2020-03-07 17:03:21 +0500

    new test whis serial port in python

--- kvadric/find_port_arduino/answer_arduino/answer_arduino.ino
+++ kvadric/find_port_arduino/answer_arduino/answer_arduino.ino
@@ -8,10 +8,15 @@ byte b = 0;
 
 void loop() 
 {
+  b = 2;
+  Serial.print('1');
+  //b = 0;
+ 
+  /*
  if(Serial.available > 0)
  {
   b = Serial.read();
   Serial.println(b);
  }
-
+ */
 }
--- kvadric/find_port_arduino/quest_python.py
+++ kvadric/find_port_arduino/quest_python.py
@@ -3,30 +3,50 @@ import sys
 from serial import Serial, SerialException
 
 PORTS = ['/dev/ttyUSB0', '/dev/ttyUSB1', '/dev/ttyUSB2', '/dev/ttyUSB3', '/dev/ttyUSB4']
-#arduino = Serial('/dev/ttyUSB0', 9600, timeout =10) 
-class FIND_ARDUINO():
+
+
+class Arduino():
     
-    def __init__(self):
+    def __init__(self, **kwargs):
         self.findport = None
+        
         for port in PORTS:
             try:
-                print(port)
-                self.findport = Serial(port = port, baudrate = 9600, timeout = 2)
-                print(self.findport.readlines())
-                break
+                #print(port)
+                self.port = Serial(port = port, baudrate = 9600, timeout = 2)
+                #print(self.port.readlines())
+                b = self.port.read()
+                left = bytes('2', encoding = "ASCII")
+                right = bytes('1', encoding = "ASCII")
+                
+                if b == right:
+                    print('Arduino in right', b, self.port)
+                elif b == left:
+                    print('Arduino in left', b, self.port)
+                else:
+                    break
+                
             except SerialException as e:
                 pass
-        if self.findport == None:
+            
+        if self.port == None:
             raise SerialException('Port is not found')
     
     def __del__(self):
         try:
-            self.findport.close()
+            self.port.close()
         except AttributeError:
             print('nicht close', file = sys.stderr)
 
+    def __str__(self):
+        return 'Arduino!!!!! ' + str(self.port)
+    
 if __name__ == '__main__':
-    port_arduino = FIND_ARDUINO()
-    del port_arduino
+    
+    arduino = Arduino()
+    
+    #print(arduino.port.readline())    
+    del arduino
+
     
      
