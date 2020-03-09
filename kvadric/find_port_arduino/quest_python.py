cb0bf94e01ba3dcac21f7397e25b8f789f7d9572
honepa <cot1884@gmail.com>
2020-03-09 19:28:23 +0500

    find arduino is relised

--- kvadric/find_port_arduino/quest_python.py
+++ kvadric/find_port_arduino/quest_python.py
@@ -7,24 +7,19 @@ PORTS = ['/dev/ttyUSB0', '/dev/ttyUSB1', '/dev/ttyUSB2', '/dev/ttyUSB3', '/dev/t
 
 class Arduino():
     
-    def __init__(self, **kwargs):
+    def __init__(self, x):
         self.port = None
-        
         for port in PORTS:
             try:
                 #print(port)
                 self.port = Serial(port = port, baudrate = 9600, timeout = 2)
                 #print(self.port.readlines())
                 b = self.port.read()
-                left = bytes('2', encoding = "ASCII")
-                right = bytes('1', encoding = "ASCII")
                 
-                if b == right:
-                    print('Arduino in right', b, self.port)
-                elif b == left:
-                    print('Arduino in left', b, self.port)
-                else:
-                    break
+                
+                if b == x:
+                    break               
+               
                 
             except SerialException as e:
                 print(port, 'failed')
@@ -39,14 +34,15 @@ class Arduino():
             print('nicht close', file = sys.stderr)
 
     def __str__(self):
-        return 'Arduino!!!!! ' + str(self.port)
+        return str(self.port)
     
 if __name__ == '__main__':
     
-    arduino = Arduino()
-    
+    left = Arduino(1)
+    right = Arduino(2)
+    print('L=%s\nR=%s' % (left,right))
     #print(arduino.port.readline())    
-    del arduino
+    del left, right
 
     
      
