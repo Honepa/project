ff5bb1338098de4dacb99ec4527887d7bc11861b
orangepi_2106 <cot1884@gmail.com>
2020-05-22 08:10:11 +0000

    new gpio

--- 2106/python_gpio/drive_test.py
+++ 2106/python_gpio/drive_test.py
@@ -16,33 +16,36 @@ gpio_pin_16_mode.write("out")
 gpio_pin_18_mode.write("out")
 
 gpio_pin_16_val = open("/sys/class/gpio/gpio16/value", "w")
-gpio_pin_18_val = open("/sys/class/gpio/gpio16/value", "w")
+gpio_pin_18_val = open("/sys/class/gpio/gpio18/value", "w")
 try:
     while 1:
         state = input()
         if(state):
+            print(state)
             gpio_pin_16_val.write("1")
             gpio_pin_18_val.write("0")
             sleep(5)
             gpio_pin_16_val.write("0")
             gpio_pin_18_val.write("0")
         else:
+            print(state)
             gpio_pin_16_val.write("0")
             gpio_pin_18_val.write("1")
             sleep(5)
             gpio_pin_16_val.write("0")
             gpio_pin_18_val.write("0")
 except KeyboardInterrupt:
-    gpio_exit = open("/sys/class/gpio/unexport", "w")
-    gpio_exit.write("16")
-    
+    gpio_exit_16 = open("/sys/class/gpio/unexport", "w")
+    gpio_exit_18 = open("/sys/class/gpio/unexport", "w")
+    gpio_exit_16.write("16")
+    gpio_exit_18.write("18")
  #   gpiosetup.close()
     gpio_pin_16_mode.close()
     gpio_pin_18_mode.close()
     gpio_pin_16_val.close()
     gpio_pin_18_val.close()
-    gpio_exit.close()
-    
+    gpio_exit_16.close()
+    gpio_exit_18.close()
     print("bye")
             
         
