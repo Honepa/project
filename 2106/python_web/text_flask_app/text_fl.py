b04181b7c7c08f7846187ca3e7f8f92677483c98
Honepa <cot1884@gmail.com>
2020-06-30 01:42:14 +0500

    error 404

--- 2106/python_web/text_flask_app/text_fl.py
+++ 2106/python_web/text_flask_app/text_fl.py
@@ -21,6 +21,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('eror404.html'), 404
+
 @app.route('/submit', methods=['POST'])
 def submit():
     code = open('code.py', "w")
